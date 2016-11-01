701c5a6ed2adc74d2799c23eb05f2c3f73159f85
Philipp Wüthrich <philipp.wuethrich@bbcag.ch>
2016-11-01 09:28:15 +0100

    ADDED: Editor layout

--- BombermanEditor/src/view/Editor.java
+++ BombermanEditor/src/view/Editor.java
@@ -3,6 +3,14 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Editor {
 
@@ -36,8 +44,24 @@ public class Editor {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 742, 524);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel Links = new JPanel();
+		frame.getContentPane().add(Links, BorderLayout.WEST);
+		Links.setLayout(new BorderLayout(0, 0));
+		
+		JPanel Toolbox = new JPanel();
+		Links.add(Toolbox, BorderLayout.NORTH);
+		Toolbox.setLayout(new CardLayout(0, 0));
+		
+		JPanel Buttons = new JPanel();
+		Links.add(Buttons, BorderLayout.SOUTH);
+				
+		JPanel Feld = new JPanel();
+		frame.getContentPane().add(Feld, BorderLayout.CENTER);
+		Feld.setLayout(new GridLayout(1, 0, 0, 0));
 	}
 
 }
